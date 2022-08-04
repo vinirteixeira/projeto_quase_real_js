8b6c7b0c0ce2c0a67b5ddf826ce2d1097178efaf
Vinícius R. Teixeira <vinicius_r_teixeira@hotmail.com>
2022-08-03 22:08:15 -0300

    atualização em sala

--- assets/js/api_github.js
+++ assets/js/api_github.js
@@ -1,19 +1,30 @@
 //exibir_load(true);
+const ulListaSeguidores = document.getElementById('listaSeguidores');
+const ulListaSeguindo = document.getElementById('listaSeguindo');
+const ulListaRepositories = document.getElementById('listaRepositories')
 
 function renderiza_foto(foto){
     const img_foto = document.getElementById('foto');
     img_foto.src=`${foto}`
+    
 }
-fetch("https://api.github.com/users/vinirteixeira")
+
+const headers = new Headers();
+    headers.append('Authorization', 'token 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',  );
+
+fetch("https://api.github.com/users/vinirteixeira", {headers:headers})
 .then(response => response.json()) // para status 200
 .then(data => {
     console.log(data);
     renderiza_foto(data.avatar_url);
     document.getElementById('nome').innerText = data.name;
     document.getElementById('location').innerHTML = "Localização: " + data.location;
-    document.getElementById('seguindo').innerHTML = "Seguindo " + data.following + " pessoas: ";
-    document.getElementById('seguidores').innerHTML = "Sendo seguido por " + data.followers + " pessoas: ";
-    document.getElementById('repositories').innerHTML = "Total de repositórios (públicos) = " +data.public_repos + ": ";
+    document.getElementById('seguindo').prepend("Seguindo " + data.following + " pessoas: ") ;
+    document.getElementById('seguidores').prepend("Sendo seguido por " + data.followers + " pessoas: ") ;
+    document.getElementById('repositories').prepend ("Total de repositórios (públicos) = " + data.public_repos + ": ");
+    getseguidores();
+    getseguindo();
+    getRepo();
 })
 
     
@@ -24,3 +35,37 @@ fetch("https://api.github.com/users/vinirteixeira")
     // exibir_load(false)
     console.warn('sempre cai aqui');
 })
+
+function getseguidores() {
+    
+    fetch("https://api.github.com/users/vinirteixeira/followers", {headers:headers})
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+           for(let elemento of data) {
+            document.getElementById('listaSeguidores').innerHTML  += `<a href="${elemento.html_url}"><li>${elemento.login}</li></a>`;
+        } 
+    })
+}
+
+function getseguindo() {
+    fetch("https://api.github.com/users/vinirteixeira/following", {headers:headers})
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+           for(let elemento of data) {
+            document.getElementById('listaSeguindo').innerHTML  += `<a href="${elemento.html_url}"><li>${elemento.login}</li><a>`;
+        } 
+    })
+}
+
+function getRepo() {
+    fetch("https://api.github.com/users/vinirteixeira/repos", {headers:headers})
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+           for(let elemento of data) {
+            document.getElementById('listaRepositories').innerHTML  += `<a href="${elemento.html_url}"><li>${elemento.name}</li></a>`;
+        } 
+    })
+}
